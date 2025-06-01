b4d40913492341878e9e7994e77dfc5b918ce34d
Advaita <135300205+A-dvaita@users.noreply.github.com>
2025-06-01 10:28:20 +0530

    Redefine operator variable and create operate funciton
    Remove the null assignment from the operator variable and create a function called operate() which calls the required mathematical calculation function depending on the operator inputted and the numbers inputted.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let num1 = 0;
 let num2 = 0;
-let operator ='';
+let operator;
 
 function add(a, b){
     return a + b;
@@ -16,4 +16,23 @@ function multiply(a, b){
 
 function divide(a, b){
     return a/b;
+}
+
+function operate(operator, num1, num2){
+    let result;
+    switch(operator){
+        case '+':
+            result = add(num1, num2);
+            break;
+        case '-':
+            result = subtract(num1, num2);
+            break;
+        case '*':
+            result = multiply(num1, num2);
+            break;
+        case '/':
+            result = divide(num1, num2);
+            break;
+    }
+    return result;
 }
